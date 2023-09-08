c8fa4b6793a62fdbce79edd2a5615360a490a785
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2023-09-08 17:12:44 +0200

    ADD: CPP-00 ex00

--- CPP-00/ex00/megaphone.cpp
+++ CPP-00/ex00/megaphone.cpp
@@ -6,7 +6,30 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 14:38:43 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/08 14:38:44 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/09/08 17:04:22 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+
+int	main(int ac, char **av)
+{
+	int	i = 1;
+
+	if (ac < 2)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		while (av[i])
+		{
+			std::string sentence (av[i]);
+
+			for (std::string::iterator str=sentence.begin(); str!=sentence.end(); ++str)
+				std::cout << (char)toupper(*str);
+			i++;
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
