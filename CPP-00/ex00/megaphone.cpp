1d2d55de9206bf5041762c68993060dbbf9c3537
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2023-09-09 13:41:33 +0200

    FIX: ++str to str++

--- CPP-00/ex00/megaphone.cpp
+++ CPP-00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/08 14:38:43 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/08 17:04:22 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/09/09 13:38:00 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,7 @@ int	main(int ac, char **av)
 		{
 			std::string sentence (av[i]);
 
-			for (std::string::iterator str=sentence.begin(); str!=sentence.end(); ++str)
+			for (std::string::iterator str=sentence.begin(); str!=sentence.end(); str++)
 				std::cout << (char)toupper(*str);
 			i++;
 		}
