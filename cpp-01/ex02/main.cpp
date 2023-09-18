0e32ca42f5ee7bbeb563862544289c4bb56aa77a
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2023-09-18 13:32:46 +0200

    ADD: cpp-01/ex02

---
+++ cpp-01/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/18 13:11:33 by jmoutous          #+#    #+#             */
+/*   Updated: 2023/09/18 13:31:16 by jmoutous         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main( void ){
+	std::string	brain = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &brain;
+	std::string	&stringREF = brain;
+
+	std::cout << "Memory address of the string brain : " << &brain << std::endl;
+	std::cout << "Memory address held by stringPTR   : " << stringPTR << std::endl;
+	std::cout << "Memory address held by stringREF   : " << &stringREF << std::endl;
+
+	std::cout << "\nValue of the string brain     : " << brain << std::endl;
+	std::cout << "Value pointed to by stringPTR : " << *stringPTR << std::endl;
+	std::cout << "Value pointed to by stringREF : " << stringREF << std::endl;
+}
