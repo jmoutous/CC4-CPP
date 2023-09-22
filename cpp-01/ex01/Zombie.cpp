a507f5107a85733485d8e948454bc1c98d10b3a3
jmoutous <jmoutous@student.42lyon.fr>
2023-09-22 17:53:00 +0200

    FIX: cpp-01/ex01 improved readability

--- cpp-01/ex01/Zombie.cpp
+++ cpp-01/ex01/Zombie.cpp
@@ -6,26 +6,26 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/15 11:10:19 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/18 13:05:52 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/09/22 17:51:24 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 Zombie::Zombie() : name("Zombie") {
-	std::cout << "Zombie constructor called" << std::endl;
+	std::cout << "Zombie's default constructor called\n" << std::endl;
 }
 
 Zombie::Zombie(std::string	name) {
-	std::cout << "Zombie constructor called for " << name << std::endl;
+	std::cout << "Zombie constructor called for " << name << "\n" << std::endl;
 	this->name = name;
 }
 
 Zombie::~Zombie() {
 	if (this->name != "Zombie")
-		std::cout << "Zombie destructor called for " << name << std::endl;
+		std::cout << "\nZombie destructor called for " << name << std::endl;
 	else
-		std::cout << "Zombie destructor called" << std::endl;
+		std::cout << "\nZombie destructor called" << std::endl;
 }
 
 void	Zombie::announce( void ) {
