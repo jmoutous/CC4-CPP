af5f9164a6fbc516f066dd73182dd52d0c2d5765
jmoutous <jmoutous@student.42lyon.fr>
2023-09-22 17:04:46 +0200

    ADD: Harl 2.0

---
+++ cpp-01/ex05/Harl.cpp
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 15:07:39 by jmoutous          #+#    #+#             */
+/*   Updated: 2023/09/22 17:02:03 by jmoutous         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(){
+	std::cout << "Harl's default constructor called\n" << std::endl;
+}
+
+Harl::~Harl(){
+	std::cout << "\nHarl's default destructor called" << std::endl;
+}
+
+void	Harl::complain( std::string level ) const {
+	std::string	message[] = {"debug", "info", "warning", "error"};
+	void		(Harl::*function[4])(void) const = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+
+	for (int i = 0; i < 4; i++) {
+		if(level == message[i]) {
+			(this->*(function[i]))();
+			return;
+		}
+	}
+	std::cerr << "Error: invalid option" << std::endl;
+}
---
+++ cpp-01/ex05/main.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/22 15:07:17 by jmoutous          #+#    #+#             */
+/*   Updated: 2023/09/22 16:50:27 by jmoutous         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+int	main(int ac, char** av) {
+	if(ac != 2) {
+		std::cerr << "Error: Usage: ./Harl <string>\nstring option: 'debug', 'info', 'warning' or 'error'" << std::endl;
+		return 1;
+	}
+
+	std::string	level = av[1];
+	Harl		test;
+
+	test.complain(level);
+}
