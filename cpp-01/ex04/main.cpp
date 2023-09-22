9a17f2ccbb0b399212d25c4af736585dd434f26f
jmoutous <jmoutous@student.42lyon.fr>
2023-09-22 14:50:24 +0200

    FIX: add protection if file does't exist

--- cpp-01/ex04/main.cpp
+++ cpp-01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 14:23:58 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/22 13:47:44 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/09/22 14:46:32 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,22 +15,25 @@
 #include <fstream>
 
 int	main(int ac, char **av){
-	if (ac != 4){
-		std::cerr << "Error:\nUsage: <filename> <string1> <string2>" << std::endl;
+	if (ac != 4 || !av[2][0] || !av[3][0]){
+		std::cerr << "Error:\nUsage: <filename> <string1> <string2>\n";
+		std::cerr << "(strings can't be empty)" << std::endl;
 		return 1;
 	}
 	
 	std::ifstream	infile(av[1]);
+
+	if(infile.fail()) {
+		std::cerr << "Error:\nEnable to open the file " << av[1] << std::endl;
+		return 1;
+	}
+
 	std::string		outfile_name = av[1];
 	std::string		str1 = av[2];
 	std::string		str2 = av[3];
+	std::ofstream	outfile(outfile_name += ".replace");
 	std::string		line;
 	std::size_t		found;
-
-	outfile_name += ".replace";
-	std::cout << outfile_name << std::endl;
-	
-	std::ofstream	outfile(outfile_name);
 
 	while (getline(infile, line)) {
 		found = line.find(str1);
