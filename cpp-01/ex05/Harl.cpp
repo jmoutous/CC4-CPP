ffc2e29a4bcbd50fa529e59a81d27b74cd87b900
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2023-09-26 13:26:21 +0200

    FIX: move function implementation in Harl.cpp and move typedef in Harl.hpp

--- cpp-01/ex05/Harl.cpp
+++ cpp-01/ex05/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 15:07:39 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/22 21:49:48 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/09/26 13:23:51 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,9 +20,21 @@ Harl::~Harl(){
 	std::cout << "\nHarl's default destructor called" << std::endl;
 }
 
+void	Harl::debug( void ) const {
+	std::cout << "DEBUG:\n\"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\"" << std::endl;
+}
+void	Harl::info( void ) const {
+	std::cout << "INFO:\n\"I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\""<< std::endl;
+}
+void	Harl::warning( void ) const {
+	std::cout << "WARNING:\n\"I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\"" << std::endl;
+}
+void	Harl::error( void ) const {
+	std::cout << "ERROR:\n\"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
+}
+
 void	Harl::complain( std::string level ) const {
-	typedef void	(Harl::*function[4])(void) const;
-	function 		f = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+	function 		f[4] = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
 	std::string		message[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
 
 	for (int i = 0; i < 4; i++) {
--- cpp-01/ex05/Harl.hpp
+++ cpp-01/ex05/Harl.hpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 15:07:35 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/22 16:58:37 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/09/26 13:23:49 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,23 +18,16 @@
 class Harl
 {
 	private:
-		void	debug( void ) const {
-			std::cout << "DEBUG:\n\"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\"" << std::endl;
-		}
-		void	info( void ) const {
-			std::cout << "INFO:\n\"I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\""<< std::endl;
-		}
-		void	warning( void ) const {
-			std::cout << "WARNING:\n\"I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\"" << std::endl;
-		}
-		void	error( void ) const {
-			std::cout << "ERROR:\n\"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
-		}
+		void	debug( void ) const;
+		void	info( void ) const;
+		void	warning( void ) const;
+		void	error( void ) const;
 
 	public:
 		Harl();
 		~Harl();
-		void	complain( std::string level ) const;
+		void			complain( std::string level ) const;
+		typedef void	(Harl::*function)(void) const;
 };
 
 #endif
