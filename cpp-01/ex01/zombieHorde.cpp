89beca4f82d78f82e8a793df1844217bffa7c7e9
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2023-09-26 14:34:07 +0200

    FIX: move zombieHorde in his own cpp file

--- cpp-01/ex01/zombieHorde.cpp
+++ cpp-01/ex01/zombieHorde.cpp
@@ -1,18 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   newZombie.cpp                                      :+:      :+:    :+:   */
+/*   zombieHorde.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/15 11:11:08 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/26 12:40:01 by jmoutous         ###   ########lyon.fr   */
+/*   Created: 2023/09/26 12:39:52 by jmoutous          #+#    #+#             */
+/*   Updated: 2023/09/26 12:40:14 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-Zombie*	newZombie( std::string name ) {
-	Zombie	*new_zombie = new Zombie(name);
-	return new_zombie;
+Zombie*	zombieHorde( int N, std::string name ) {
+	Zombie	*horde;
+
+	if (N <= 0)
+		return (NULL);
+	
+	horde = new Zombie[N];
+	for(int i = 0; i < N ; i++) {
+		horde[i].setName(name);
+	}
+
+	return horde;
 }
