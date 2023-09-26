c82af648486571e354936266be81f407182c828e
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2023-09-26 14:19:40 +0200

    FIX: move function implementation in .hpp, typo in .hpp and use switch in .cpp

--- cpp-01/ex06/Harl.cpp
+++ cpp-01/ex06/Harl.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 15:07:39 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/22 21:51:09 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/09/26 14:18:36 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,19 +20,43 @@ Harl::~Harl(){
 	std::cout << "\nHarl's default destructor called" << std::endl;
 }
 
-void	Harl::complain( std::string level ) const {
-	typedef void	(Harl::*function[4])(void) const;
-	function 		f = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
-	std::string		message[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+void	Harl::debug( void ) const {
+	std::cout << "\n[ DEBUG ]\n\"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\"" << std::endl;
+}
 
-	for (int i = 0; i < 4; i++) {
+void	Harl::info( void ) const {
+	std::cout << "\n[ INFO ]\n\"I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\""<< std::endl;
+}
+
+void	Harl::warning( void ) const {
+	std::cout << "\n[ WARNING ]\n\"I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\"" << std::endl;
+}
+
+void	Harl::error( void ) const {
+	std::cout << "\n[ ERROR ]\n\"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
+}
+
+void	Harl::complain( std::string level ) const {
+	std::string		message[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+	int				i = 0;
+
+	for (; i < 4; i++) {
 		if(level == message[i]) {
-			while (i < 4) {
-				(this->*(f[i]))();
-				i++;
-			}
-			return;
+			break;
 		}
 	}
-	std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+
+	switch (i) {
+		case 0:
+			Harl::debug();
+		case 1:
+			Harl::info();
+		case 2:
+			Harl::warning();
+		case 3:
+			Harl::error();
+			break;
+		default:
+			std::cout << "[ Probably complaining about insignificant problems ]" << std::endl;
+	}
 }
--- cpp-01/ex06/Harl.hpp
+++ cpp-01/ex06/Harl.hpp
@@ -6,11 +6,11 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 15:07:35 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/09/22 17:22:04 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/09/26 13:37:36 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef HARL__HPP
+#ifndef HARL_HPP
 # define HARL_HPP
 
 # include <iostream>
@@ -18,18 +18,10 @@
 class Harl
 {
 	private:
-		void	debug( void ) const {
-			std::cout << "\n[ DEBUG ]\n\"I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!\"" << std::endl;
-		}
-		void	info( void ) const {
-			std::cout << "\n[ INFO ]\n\"I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn't be asking for more!\""<< std::endl;
-		}
-		void	warning( void ) const {
-			std::cout << "\n[ WARNING ]\n\"I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month.\"" << std::endl;
-		}
-		void	error( void ) const {
-			std::cout << "\n[ ERROR ]\n\"This is unacceptable! I want to speak to the manager now.\"" << std::endl;
-		}
+		void	debug( void ) const;
+		void	info( void ) const;
+		void	warning( void ) const;
+		void	error( void ) const;
 
 	public:
 		Harl();
