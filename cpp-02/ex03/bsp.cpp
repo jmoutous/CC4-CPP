daee09f3c0174ee2e853dde26d30e9dd9ebf920f
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2023-10-02 19:24:29 +0200

    ADD: cpp-02/ex03/ function for the BSP

--- cpp-02/ex03/bsp.cpp
+++ cpp-02/ex03/bsp.cpp
@@ -6,18 +6,34 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/02 16:25:32 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/10/02 17:03:33 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/10/02 19:23:03 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Fixed.hpp"
 #include "Point.hpp"
 
-bool bsp( Point const a, Point const b, Point const c, Point const point) {
-	(void) a;
-	(void) b;
-	(void) c;
-	(void) point;
+int	orientation(Point const point, Point const a, Point const b) {
+	float	cross_product;
 
-	return false;
+	cross_product = ((point.getX() - a.getX()) * (point.getY() - b.getY()))
+					- ((point.getY() - a.getY()) * (point.getX() - b.getX()));
+
+	if( cross_product > 0)
+		return (1);
+	else
+		return (0);
+}
+
+bool bsp(Point const a, Point const b, Point const c, Point const point) {
+	int	o1, o2, o3;
+
+	o1 = orientation(point, a, b);
+	o2 = orientation(point, b, c);
+	o3 = orientation(point, c, a);
+
+	if (o1 == o2 && o2 == o3)
+		return true;
+	else
+		return false;
 }
--- cpp-02/ex03/main.cpp
+++ cpp-02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/22 22:10:54 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/10/02 18:09:34 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/10/02 19:22:32 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,17 @@
 bool bsp( Point const a, Point const b, Point const c, Point const point);
 
 int	main(void) {
-	// {
-	// 	Point const	a(1,1);
-	// 	Point const	b(6,1);
-	// 	Point const	c(1,6);
-	// 	Point const	point(2,2);
+	{
+		Point const	a(2,1);
+		Point const	b(4,2);
+		Point const	c(0,6);
+		Point const	point(1,3.4f);
 
-	// 	if(bsp(a, b, c, point))
-	// 		std::cout << "Le point se trouve dans le triangle" << std::endl;
-	// 	else
-	// 		std::cout << "Le point ne se trouve pas dans le triangle" << std::endl;
-	// }
+		if(bsp(a, b, c, point))
+			std::cout << "Le point se trouve dans le triangle" << std::endl;
+		else
+			std::cout << "Le point ne se trouve pas dans le triangle" << std::endl;
+	}
 
 	// {
 	// 	Point const	a(2,1);
