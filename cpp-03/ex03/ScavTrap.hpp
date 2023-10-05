6c69ea35173eed11b766b504405ae11265b1d0fc
Julien Moutoussamy <julien.moutoussamy@gmail.com>
2023-10-05 14:00:02 +0200

    FIX: cpp-03/ex03 ScavTrap class take a virtual public ClapTrap

--- cpp-03/ex03/ScavTrap.hpp
+++ cpp-03/ex03/ScavTrap.hpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 13:11:00 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/10/05 13:08:28 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/10/05 13:58:28 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include "ClapTrap.hpp"
 
-class ScavTrap : public ClapTrap {
+class ScavTrap : virtual public ClapTrap {
 	public:
 		ScavTrap();
 		ScavTrap( std::string name );
