594ba428f92b9f56bee80f60468cd8de7f4d38d8
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-10-13 14:54:51 +0200

    ADD: cpp-04/ex01/ add subject's test

--- cpp-04/ex01/main.cpp
+++ cpp-04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 10:38:14 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/10/13 14:50:52 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/10/13 14:54:05 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -77,27 +77,38 @@ int	main( void ) {
 
 	// 	std::cout << std::endl;
 	// }
+	// {
+	// 	Dog	a,b;
+
+	// 	std::cout << std::endl;
+
+	// 	a.setIdeas(0, "I like food");
+	// 	a.setIdeas(1, "I like to walk outside");
+	// 	std::cout << "Brain a: idea[0] = " << a.getIdeas(0) << std::endl;
+	// 	std::cout << "Brain b: idea[0] = " << b.getIdeas(0) << std::endl;
+	// 	std::cout << "Brain a: idea[1] = " << a.getIdeas(1) << std::endl;
+	// 	std::cout << "Brain b: idea[1] = " << b.getIdeas(1) << std::endl;
+
+	// 	std::cout << std::endl;
+
+	// 	b = a;
+	// 	std::cout << "Brain a: idea[0] = " << a.getIdeas(0) << std::endl;
+	// 	std::cout << "Brain b: idea[0] = " << b.getIdeas(0) << std::endl;
+	// 	std::cout << "Brain a: idea[1] = " << a.getIdeas(1) << std::endl;
+	// 	std::cout << "Brain b: idea[1] = " << b.getIdeas(1) << std::endl;
+
+	// 	std::cout << std::endl;
+	// }
 	{
-		Dog	a,b;
+		std::cout << "====== Subject's test ======\n" << std::endl;
 
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		
 		std::cout << std::endl;
 
-		a.setIdeas(0, "I like food");
-		a.setIdeas(1, "I like to walk outside");
-		std::cout << "Brain a: idea[0] = " << a.getIdeas(0) << std::endl;
-		std::cout << "Brain b: idea[0] = " << b.getIdeas(0) << std::endl;
-		std::cout << "Brain a: idea[1] = " << a.getIdeas(1) << std::endl;
-		std::cout << "Brain b: idea[1] = " << b.getIdeas(1) << std::endl;
-
-		std::cout << std::endl;
-
-		b = a;
-		std::cout << "Brain a: idea[0] = " << a.getIdeas(0) << std::endl;
-		std::cout << "Brain b: idea[0] = " << b.getIdeas(0) << std::endl;
-		std::cout << "Brain a: idea[1] = " << a.getIdeas(1) << std::endl;
-		std::cout << "Brain b: idea[1] = " << b.getIdeas(1) << std::endl;
-
-		std::cout << std::endl;
+		delete j;//should not create a leak
+		delete i;
 	}
 
 	return (0);
