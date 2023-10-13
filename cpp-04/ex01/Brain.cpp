b11f52981b0f4cad13da75b1eb4496ed542d2d1e
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-10-13 13:57:50 +0200

    FIX: cpp-04/ex01/ operator= of the Brain

--- cpp-04/ex01/Brain.cpp
+++ cpp-04/ex01/Brain.cpp
@@ -6,37 +6,40 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 13:40:56 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/10/12 16:23:22 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/10/13 13:55:34 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Brain.hpp"
 
 Brain::Brain() {
-    std::cout << "Brain's default constructor called" << std::endl;
+	std::cout << "Brain's default constructor called" << std::endl;
 };
 
 Brain::Brain( Brain const & source ) {
-    std::cout << "Brain's copy constructor called" << std::endl;
+	std::cout << "Brain's copy constructor called" << std::endl;
 
-        *this = source;
+		*this = source;
 };
 
 Brain::~Brain() {
-    std::cout << "Brain's destructor called" << std::endl;
+	std::cout << "Brain's destructor called" << std::endl;
 };
 
 Brain & Brain::operator=( Brain const & rhs ) {
-    if (this != &rhs)
-        *this->_ideas = *rhs._ideas;
+    if (this != &rhs) {
+		for(int i = 0; i < 100; i++) {
+			this->_ideas[i] = rhs._ideas[i];
+		}
+	}
 
-    return (*this);
+	return (*this);
 };
 
 void		Brain::setIdeas( int ideaNb, std::string oneIdea ) {
-    this->_ideas[ideaNb] = oneIdea;
+	this->_ideas[ideaNb] = oneIdea;
 }
 
 std::string	Brain::getIdeas( int ideaNb ) {
-    return (this->_ideas[ideaNb]);
+	return (this->_ideas[ideaNb]);
 }
