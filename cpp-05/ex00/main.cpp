5c86e469dcfd9cc96b2dc95462f91f7cd538d5e8
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-11-03 15:17:51 +0100

    FIX: cpp-05/ex00/ better main

--- cpp-05/ex00/main.cpp
+++ cpp-05/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 17:27:30 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/11/03 15:12:44 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/11/03 15:17:16 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,31 @@
 
 int	main()
 {
-	// {
-	// 	Bureaucrat	a("Julien", 50);
+	{
+		Bureaucrat	a("Julien", 50);
 
-	// 	std::cout << "\n" << a << std::endl;
+		std::cout << "\n" << a << std::endl;
 
-	// 	a.incrementGrade();
+		std::cout << "a.incrementGrade()";
+		a.incrementGrade();
 
-	// 	std::cout << "\n" << a << std::endl;
+		std::cout << "\n" << a << std::endl;
 
-	// 	a.decrementGrade();
+		std::cout << "a.decrementGrade()";
+		a.decrementGrade();
 
-	// 	std::cout << "\n" << a << std::endl;
-	// }
+		std::cout << "\n" << a << std::endl;
+
+		std::cout << "a.incrementGrade(49)";
+		a.incrementGrade(49);
+
+		std::cout << "\n" << a << std::endl;
+
+		std::cout << "a.decrementGrade(149)";
+		a.decrementGrade(149);
+
+		std::cout << "\n" << a << std::endl;
+	}
 
 	// {
 	// 	Bureaucrat	a("Julien", 50);
@@ -45,22 +57,22 @@ int	main()
 	// 	std::cout << "\n" << a << std::endl;
 	// }
 
-	{
-		Bureaucrat	a("Julien", 50);
+	// {
+	// 	Bureaucrat	a("Julien", 50);
 
-		std::cout << "\n" << a << std::endl;
+	// 	std::cout << "\n" << a << std::endl;
 
-		try
-		{
-			a.incrementGrade(101);
-		}
-		catch (std::exception & e)
-		{
-			std::cout << e.what() << std::endl;
-		}
+	// 	try
+	// 	{
+	// 		a.incrementGrade(101);
+	// 	}
+	// 	catch (std::exception & e)
+	// 	{
+	// 		std::cout << e.what() << std::endl;
+	// 	}
 		
-		std::cout << "\n" << a << std::endl;
-	}
+	// 	std::cout << "\n" << a << std::endl;
+	// }
 
 	return (0);
 }
