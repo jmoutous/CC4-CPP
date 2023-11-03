61a14691890a2bb684a860f7152f8ecfac7ee1f9
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-11-03 16:08:44 +0100

    FIX: cpp-05/ex00/ better main

--- cpp-05/ex00/main.cpp
+++ cpp-05/ex00/main.cpp
@@ -6,39 +6,66 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 17:27:30 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/11/03 15:17:16 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/11/03 16:06:07 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include <stdexcept>
 
 int	main()
 {
 	{
-		Bureaucrat	a("Julien", 50);
+		try
+		{
+			Bureaucrat	a("Julien", 1150);
 
-		std::cout << "\n" << a << std::endl;
+			std::cout << "\n" << a << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
+		
+		std::cout << std::endl;
 
-		std::cout << "a.incrementGrade()";
-		a.incrementGrade();
+		try
+		{
+			Bureaucrat	a("Julien", 0);
 
-		std::cout << "\n" << a << std::endl;
-
-		std::cout << "a.decrementGrade()";
-		a.decrementGrade();
-
-		std::cout << "\n" << a << std::endl;
-
-		std::cout << "a.incrementGrade(49)";
-		a.incrementGrade(49);
-
-		std::cout << "\n" << a << std::endl;
-
-		std::cout << "a.decrementGrade(149)";
-		a.decrementGrade(149);
-
-		std::cout << "\n" << a << std::endl;
+			std::cout << "\n" << a << std::endl;
+		}
+		catch (std::exception & e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 	}
+
+	// {
+	// 	Bureaucrat	a("Julien", 50);
+
+	// 	std::cout << "\n" << a << std::endl;
+
+	// 	std::cout << "a.incrementGrade()";
+	// 	a.incrementGrade();
+
+	// 	std::cout << "\n" << a << std::endl;
+
+	// 	std::cout << "a.decrementGrade()";
+	// 	a.decrementGrade();
+
+	// 	std::cout << "\n" << a << std::endl;
+
+	// 	std::cout << "a.incrementGrade(49)";
+	// 	a.incrementGrade(49);
+
+	// 	std::cout << "\n" << a << std::endl;
+
+	// 	std::cout << "a.decrementGrade(149)";
+	// 	a.decrementGrade(149);
+
+	// 	std::cout << "\n" << a << std::endl;
+	// }
 
 	// {
 	// 	Bureaucrat	a("Julien", 50);
