f76d043b1b4cec9ef7d249c5eb8b07e2994e19cd
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-11-06 10:05:01 +0100

    ADD: cpp-05/ex01/ better main

--- cpp-05/ex01/main.cpp
+++ cpp-05/ex01/main.cpp
@@ -6,16 +6,35 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 17:27:30 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/11/03 15:33:24 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/11/06 10:01:09 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
+#include "Form.hpp"
 
 int	main()
 {
 	{
-	}
+		Bureaucrat	a("Julien", 34);
+		Form		b("Lul", 50, 10), c("Mdr", 10, 10);
+
+		std::cout << std::endl;
+		
+		std::cout << a << std::endl;
+		std::cout << b << std::endl;
+		std::cout << c << std::endl;
+
+		std::cout << std::endl;
+
+		a.signForm(b);
+		a.signForm(c);
+
+		std::cout << std::endl;
+		
+		std::cout << b << std::endl;
+		std::cout << c << std::endl;
+}
 
 	return (0);
 }
