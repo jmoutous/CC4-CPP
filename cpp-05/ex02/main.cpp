346588910a54236bf0f19862e417d1086693d89d
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-11-07 14:08:48 +0100

    FIX: cpp-05/ex02/ better main

--- cpp-05/ex02/main.cpp
+++ cpp-05/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 17:27:30 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/11/07 13:44:55 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/11/07 14:08:24 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,8 @@ int	main()
 		
 	// 	std::cout << a << std::endl;
 	// }
+
+		std::cout << "\n=============== ShrubberyCreationForm ===============\n"<< std::endl;
 
 	// {
 	// 	std::cout << "\n===== Grade too low to sign the form ====="<< std::endl;
@@ -91,6 +93,75 @@ int	main()
 
 	// 	delete a;
 	// }
+
+	std::cout << "\n=============== RobotomyRequestForm ===============\n"<< std::endl;
+
+	// {
+	// 	std::cout << "\n===== Grade too low to sign the form ====="<< std::endl;
+
+	// 	Bureaucrat	ju("Julien", 150);
+	// 	AForm*	a = new RobotomyRequestForm("Brain");
+
+	// 	std::cout << "Bureaucrat " << ju.getName() << ", grade " << ju.getGrade() << std::endl;
+	// 	std::cout << *a << '\n' << std::endl;
+
+	// 	ju.signForm(*a);
+
+	// 	std::cout << std::endl;
+	// 	ju.executeForm(*a);
+
+	// 	delete a;
+	// }
+
+	// {
+	// 	std::cout << "\n===== Grade too low to execut the form ====="<< std::endl;
+
+	// 	Bureaucrat	ju("Julien", 72);
+	// 	AForm*	a = new RobotomyRequestForm("Brain");
+
+	// 	std::cout << "Bureaucrat " << ju.getName() << ", grade " << ju.getGrade() << std::endl;
+	// 	std::cout << *a << '\n' << std::endl;
+
+	// 	ju.signForm(*a);
+
+	// 	std::cout << std::endl;
+	// 	ju.executeForm(*a);
+
+	// 	delete a;
+	// }
+
+	// {
+	// 	std::cout << "\n===== Form not signed ====="<< std::endl;
+
+	// 	Bureaucrat	ju("Julien", 45);
+	// 	AForm*	a = new RobotomyRequestForm("Brain");
+
+	// 	std::cout << "Bureaucrat " << ju.getName() << ", grade " << ju.getGrade() << std::endl;
+	// 	std::cout << *a << '\n' << std::endl;
+
+	// 	ju.executeForm(*a);
+
+	// 	delete a;
+	// }
+
+	// {
+	// 	std::cout << "\n===== Everything works ====="<< std::endl;
+
+	// 	Bureaucrat	ju("Julien", 45);
+	// 	AForm*	a = new RobotomyRequestForm("Brain");
+
+	// 	std::cout << "Bureaucrat " << ju.getName() << ", grade " << ju.getGrade() << std::endl;
+	// 	std::cout << *a << '\n' << std::endl;
+
+	// 	ju.signForm(*a);
+
+	// 	std::cout << std::endl;
+	// 	ju.executeForm(*a);
+
+	// 	delete a;
+	// }
+
+	std::cout << "\n=============== PresidentialPardonForm ===============\n"<< std::endl;
 
 	// {
 	// 	Bureaucrat	ju("Julien", 1);
