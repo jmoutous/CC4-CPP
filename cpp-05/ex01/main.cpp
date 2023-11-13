a5445a8d4089327fc3f75b990c83c327860ece47
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-11-13 13:58:56 +0100

    FIX: cpp-05/ex01/ better main

--- cpp-05/ex01/main.cpp
+++ cpp-05/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/27 17:27:30 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/11/06 10:19:52 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/11/13 13:58:17 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,26 +15,98 @@
 
 int	main()
 {
+	// {
+	// 	Bureaucrat	a("Julien", 34);
+	// 	Form		b("Lul", 50, 10), c("Mdr", 10, 10);
+
+	// 	std::cout << std::endl;
+		
+	// 	std::cout << a << std::endl;
+	// 	std::cout << b << std::endl;
+	// 	std::cout << c << std::endl;
+
+	// 	std::cout << std::endl;
+
+	// 	a.signForm(b);
+	// 	a.signForm(c);
+
+	// 	std::cout << std::endl;
+		
+	// 	std::cout << b << std::endl;
+	// 	std::cout << c << std::endl;
+	// }
+
 	{
-		Bureaucrat	a("Julien", 34);
-		Form		b("Lul", 50, 10), c("Mdr", 10, 10);
-
-		std::cout << std::endl;
-		
-		std::cout << a << std::endl;
-		std::cout << b << std::endl;
-		std::cout << c << std::endl;
-
 		std::cout << std::endl;
 
-		a.signForm(b);
-		a.signForm(c);
+		try
+		{
+			Form	a("Impossible", 0, 50);
+
+			std::cout << a << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Form not created: " << e.what() << std::endl;
+		}
 
 		std::cout << std::endl;
-		
-		std::cout << b << std::endl;
-		std::cout << c << std::endl;
+
+		try
+		{
+			Form	a("Impossible", 50, 0);
+
+			std::cout << a << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Form not created: " << e.what() << std::endl;
+		}
+
+		std::cout << std::endl;
+
+		try
+		{
+			Form	a("Impossible", 200, 50);
+
+			std::cout << a << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Form not created: " << e.what() << std::endl;
+		}
+
+		std::cout << std::endl;
+
+		try
+		{
+			Form	a("Impossible", 50, 200);
+
+			std::cout << a << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << "Form not created: " << e.what() << std::endl;
+		}
+
+		std::cout << std::endl;
 	}
+
+	// {
+	// 	try
+	// 	{
+	// 		Form*	a = new Form("Impossible", 0, 50);
+
+	// 		std::cout << *a << std::endl;
+			
+	// 		delete a;
+	// 	}
+	// 	catch(const std::exception& e)
+	// 	{
+	// 		std::cerr << "Form not created: " << e.what() << '\n';
+	// 	}
+			
+	// }
 
 	return (0);
 }
