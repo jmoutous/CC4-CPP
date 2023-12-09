95aac304b5ecc9fee51a3da6d5581c88e84484a3
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-12-09 16:05:45 +0100

    FIX: cpp-06/ex00/ error msg when incorrect number of arguments

--- cpp-06/ex00/main.cpp
+++ cpp-06/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/29 09:35:58 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/11/30 10:03:48 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/12/09 16:05:16 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 int	main (int ac, char **av)
 {
+	if (ac != 2)
+	{
+		std::cout << "Usage: ./convert <char, int, float or double>" << std::endl;
+		return (1);
+	}
+
 	ScalarConverter::convert(av[1]);
-	(void) ac;
 
 	return (0);
 }
