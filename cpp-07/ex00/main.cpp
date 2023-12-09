6254b3e8259b7b0e3c834a924434adeda888e906
Julien Moutoussamy <jmoutous@z4r5p7.42lyon.fr>
2023-12-09 16:40:50 +0100

    FIX: cpp-07/ex00/ better main

--- cpp-07/ex00/main.cpp
+++ cpp-07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/30 10:54:46 by jmoutous          #+#    #+#             */
-/*   Updated: 2023/11/30 11:13:13 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2023/12/09 16:39:21 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,21 +36,65 @@ T const &	max(T const & x, T const & y)
 
 int	main( void )
 {
-	int a = 2;
-	int b = 3;
+	{
+		std::cout << "===== Subject's test =====\n" << std::endl;
 
-	::swap(a, b);
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	
-	std::string c = "chaine1";
-	std::string d = "chaine2";
+		int a = 2;
+		int b = 3;
 
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	
+		std::cout << "Before swap\t";
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "After swap \t";
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		
+		std::cout << std::endl;
+
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+
+		std::cout << "Before swap\t";
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		::swap(c, d);
+		std::cout << "After swap \t";
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+
+	{
+		std::cout << "\n===== Extra tests =====\n" << std::endl;
+
+		float a = 2.9f;
+		float b = 3.1f;
+
+		std::cout << "Before swap\t";
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		::swap(a, b);
+		std::cout << "After swap \t";
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+
+		std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+		std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+		
+		std::cout << std::endl;
+
+		long double c = 666.42;
+		long double d = 4973.21;
+
+		std::cout << "Before swap\t";
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		::swap(c, d);
+		std::cout << "After swap \t";
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+
+		std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+		std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	}
+
 	return 0;
 }
