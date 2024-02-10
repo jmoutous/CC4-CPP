b78d868625acd014fc18c4b58b6144133651d741
jmoutous <julien.moutoussamy@gmail.com>
2024-02-10 18:15:39 +0100

    UPDATE: cpp-08/ex00 add try/catch in main

--- cpp-08/ex00/main.cpp
+++ cpp-08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/01 16:16:13 by jmoutous          #+#    #+#             */
-/*   Updated: 2024/02/03 17:09:55 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2024/02/10 18:15:08 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,53 +16,57 @@
 
 int main(void)
 {
-	std::vector<int>	container;
-	int					nbs;
-	int					counter = 0;
-
-	srand(time(NULL));
-
-	std::cout << "\nEnter the number of vector's number: ";
-	do
-	{
-		std::cin >> nbs;
-		if(std::cin.fail() || nbs <= 0)
-		{
-			std::cin.clear();
-			std::cin.ignore();
-		}
-	} while (nbs == 0);
-	std::cout << std::endl;
-
-	for (int i = 0; i < nbs; ++i)
-	{
-		int randNb = rand() % 1000;
-		container.push_back(randNb);
-	}
-
-	std::cout << "The container has been filled with " << nbs << " random numbers :" << std::endl;
-	for (std::vector<int>::iterator it = container.begin(); it != container.end(); ++it)
-	{
-		std::cout << *it << "\t";
-		counter++;
-		if (counter % 10 == 0)
-			std::cout << std::endl;
-	}
-
-	std::cout << "\n\nWhich number do you want to find? ";
-	do
-	{
-		std::cin >> nbs;
-		if(std::cin.fail())
-		{
-			std::cin.clear();
-			std::cin.ignore();
-		}
-	} while (nbs == 0);
-	std::cout << std::endl;
-
 	try
 	{
+		std::vector<int>	container;
+		int					nbs;
+		int					counter = 0;
+
+		srand(time(NULL));
+
+		std::cout << "\nEnter the number of vector's number: ";
+		do
+		{
+			std::cin >> nbs;
+			if(std::cin.fail() || nbs <= 0)
+			{
+				std::cin.clear();
+				std::cin.ignore();
+				std::cout << "Invalid input: you must input only numbers" << std::endl;
+				return (1);
+			}
+		} while (nbs == 0);
+		std::cout << std::endl;
+
+		for (int i = 0; i < nbs; ++i)
+		{
+			int randNb = rand() % 1000;
+			container.push_back(randNb);
+		}
+
+		std::cout << "The container has been filled with " << nbs << " random numbers :" << std::endl;
+		for (std::vector<int>::iterator it = container.begin(); it != container.end(); ++it)
+		{
+			std::cout << *it << "\t";
+			counter++;
+			if (counter % 10 == 0)
+				std::cout << std::endl;
+		}
+
+		std::cout << "\n\nWhich number do you want to find? ";
+		do
+		{
+			std::cin >> nbs;
+			if(std::cin.fail())
+			{
+				std::cin.clear();
+				std::cin.ignore();
+				std::cout << "Invalid input: you must input only numbers" << std::endl;
+				return (1);
+			}
+		} while (nbs == 0);
+		std::cout << std::endl;
+
 		std::vector<int>::iterator	it = easyFind(container, nbs);
 
 		std::cout << *it << " is present in the container" << std::endl;
@@ -71,6 +75,6 @@ int main(void)
 	{
 		std::cout << e.what() << std::endl;
 	}
-	
+
 	return (0);
 }
