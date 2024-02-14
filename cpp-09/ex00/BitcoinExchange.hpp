40da821acca1392028e537be5f66f97d6994e237
jmoutous <julien.moutoussamy@gmail.com>
2024-02-14 17:17:08 +0100

    CLEAN: cpp09/ex00/

--- cpp-09/ex00/BitcoinExchange.hpp
+++ cpp-09/ex00/BitcoinExchange.hpp
@@ -6,7 +6,7 @@
 /*   By: jmoutous <jmoutous@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 18:10:00 by jmoutous          #+#    #+#             */
-/*   Updated: 2024/02/14 16:40:54 by jmoutous         ###   ########lyon.fr   */
+/*   Updated: 2024/02/14 17:16:27 by jmoutous         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 # include <cstdlib>
 # include <ctime>
 # include <sstream>
-						# include "log.hpp"
 
 std::map<std::string, double>	getDataBase( void ); 
 void							process( char *infile, std::map<std::string, double> & dataBase );
